cab3c1a997928a121baa72b474ae0db820662555
EugenMinerDeDate <eugencapata802@gmail.com>
2022-06-30 09:09:54 +0300

    Fml

---
+++ design-patterns-pattern_flyweight/Program.cs
@@ -0,0 +1,9 @@
+﻿using Patterns.Bars;
+
+Patterns.Flyweight.Run();
+
+BarInterface proxyBar = new BarProxy();
+Console.WriteLine("\n ----------------Proxy-----------------"); 
+Console.WriteLine(proxyBar.askForMenu());
+proxyBar.createOrder("Bere",2);
+proxyBar.payUp();
